7e2b2c3f9dc5f0d074498e857528d618ed00f2a2
Training_B6E.00.13 <Training_B6E.00.13@BDC6-DX-87R1N62.dir.svc.accenture.com>
2019-07-23 10:58:30 +0530

    my first commit

---
+++ CucumberTest/src/main/java/stepDefinition/stepDef.java
@@ -0,0 +1,13 @@
+package stepDefinition;
+
+import cucumber.api.java.en.Given;
+
+public class stepDef {
+@Given("^I want to write a step with preconditon$")
+public void I_want_to_write_a_step_with_precondition() throws Exception{
+	
+	System.out.println("Test is passed");
+	
+}
+
+}
